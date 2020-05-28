d6a6f719ca125272e36703debb2ff2678b70e03c
mattdean1 <mail@mattdean.io>
2020-05-28 16:54:46 +0100

    Add commander

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-- install node 14 using nvm
+# How to run
+
+1. Install latest version of node (14) e.g. using [nvm](https://github.com/nvm-sh/nvm)
+2. yarn run
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-const inputDataFilePath = '../data/input_data.gz'
-const outputDataFilePath = '../data/input_data'
+import commander from 'commander'
 
 import { readFile } from './readFile/index.js'
 import { enrichData } from './enrichData/index.js'
 import { aggregateMetrics } from './aggregateMetrics/index.js'
 import { storeResult } from './storeResult/index.js'
 
+const { program } = commander
+const inputDataFilePath = '../data/input_data.gz'
+const outputDataFilePath = '../data/input_data'
 
-const main = async () => {
-  // TODO: extract all files in a given directory
-  // TODO: take directory as input on command line
+const generateStats = async (inputDirectory) => {
+  console.log(inputDirectory);
 
   const stream = readFile(inputDataFilePath)
   const enrichedStream = enrichData(stream)
@@ -22,5 +23,15 @@ const main = async () => {
   console.log(metrics);
 }
 
-main()
+program
+  .version('0.0.1')
+  .description('ETL system for web events')
+  .command('generate <input-directory>')
+  .description('Generate event stats for country/city and user stats for browser/os')
+  .option('-p, --print', 'Should print stats to terminal')
+  .action((inputDirectory) => {
+    generateStats(inputDirectory)
+  })
+
+program.parse(process.argv)
 
