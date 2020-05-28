facddca7777f1c5ca0d89a50cdc85dc694b23257
mattdean1 <mail@mattdean.io>
2020-05-28 21:44:38 +0100

    Pass through cli params

--- src/cli.js
+++ src/cli.js
@@ -7,12 +7,12 @@ const { program } = commander
 program
   .version("0.0.1")
   .description("ETL system for web events")
-  .command("generate <input-directory>")
+  .command("generate <input-directory> [output-file]")
   .description(
     "Generate event stats for country/city and user stats for browser/os"
   )
   .option("-p, --print", "Should print stats to terminal")
-  .action(inputDirectory => generateStats(inputDirectory))
+  .action((inputDirectory, outputDirectory, options) => generateStats(inputDirectory, outputDirectory, options.print))
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
--- src/generateStats.js
+++ src/generateStats.js
@@ -3,15 +3,17 @@ import { enrichData } from "./enrich/index.js"
 import { aggregateMetrics } from "./aggregate/index.js"
 import { storeResult } from "./store/index.js"
 
-export const generateStats = async inputDirectory => {
+export const generateStats = async (inputDirectory, outputDirectory = '/dev/null', print = false) => {
   const stream = readFiles(inputDirectory)
 
   const enrichedStream = enrichData(stream)
 
   const [aggregatedStream, metricsPromise] = aggregateMetrics(enrichedStream)
 
-  storeResult(aggregatedStream, "/dev/null")
+  storeResult(aggregatedStream, outputDirectory)
 
   const metrics = await metricsPromise
-  console.log(metrics)
+  if (print) {
+    console.log(metrics)
+  }
 }
